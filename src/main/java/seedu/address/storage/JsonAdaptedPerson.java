25274060e64b01f823ccacb3d7f36c0ca284f763
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-10-05 23:25:20 +0800

    fix: Add githubUsername field to test JSONs

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -38,7 +38,7 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
             @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tags") List<JsonAdaptedTag> tags, @JsonProperty("github username") String githubUsername) {
+            @JsonProperty("tags") List<JsonAdaptedTag> tags, @JsonProperty("githubUsername") String githubUsername) {
         this.name = name;
         this.phone = phone;
         this.email = email;

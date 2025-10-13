d95d1464f3b657f87b6f99b8d7a12e887dbfc1ef
junhao4 <e1406393@u.nus.edu>
2025-10-13 16:58:51 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -55,7 +55,8 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
-        descriptor.getGithubUsername().ifPresent(githubUsername -> sb.append(PREFIX_GITHUB_USERNAME).append(githubUsername.value).append(" "));
+        descriptor.getGithubUsername().ifPresent(githubUsername -> sb.append(PREFIX_GITHUB_USERNAME)
+                .append(githubUsername.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {

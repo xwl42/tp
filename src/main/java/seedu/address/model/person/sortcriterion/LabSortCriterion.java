53761d7536c74001044ec429cd67df13fb145000
Sahishnu <sahishnuk2@gmail.com>
2025-10-19 19:19:20 +0800

    Add javadoc comment for Lab Criteria

--- src/main/java/seedu/address/model/person/sortcriterion/LabSortCriterion.java
+++ src/main/java/seedu/address/model/person/sortcriterion/LabSortCriterion.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 
 import seedu.address.model.person.Person;
 
+/**
+ * Sorts persons by their lab attendance in descending order.
+ */
 public class LabSortCriterion extends SortCriterion {
     public static final String CRITERION_KEYWORD = "lab";
 

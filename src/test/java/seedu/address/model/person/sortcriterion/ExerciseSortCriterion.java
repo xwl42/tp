17c5cdce578edc17658cfe17318078688bd69f15
Sahishnu <sahishnuk2@gmail.com>
2025-10-20 17:38:39 +0800

    Create a sort criterion for exercise progress

---
+++ src/test/java/seedu/address/model/person/sortcriterion/ExerciseSortCriterion.java
@@ -0,0 +1,24 @@
+package seedu.address.model.person.sortcriterion;
+
+import java.util.Comparator;
+
+import seedu.address.model.person.Person;
+
+public class ExerciseSortCriterion extends SortCriterion {
+    public static final String CRITERION_KEYWORD = "ex";
+
+    @Override
+    public Comparator<Person> getComparator() {
+        return Comparator.comparing(Person::getExerciseTracker, Comparator.reverseOrder());
+    }
+
+    @Override
+    public String getDisplayString() {
+        return "exercise progress";
+    }
+
+    @Override
+    public String toString() {
+        return CRITERION_KEYWORD;
+    }
+}

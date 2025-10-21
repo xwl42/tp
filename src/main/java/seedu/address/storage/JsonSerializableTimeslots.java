3267675ffb195ad03c41799f5a0d67c17957a256
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-10-21 13:21:34 +0800

    feat: Add other utilities for Timeslot

---
+++ src/main/java/seedu/address/storage/JsonSerializableTimeslots.java
@@ -0,0 +1,64 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import seedu.address.model.timeslot.Timeslot;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.ReadOnlyTimeslots;
+import seedu.address.model.Timeslots;
+
+/**
+ * An Immutable Timeslots that is serializable to JSON format.
+ */
+@JsonRootName(value = "timeslots")
+class JsonSerializableTimeslots {
+
+    public static final String MESSAGE_DUPLICATE_TIMESLOT = "Timeslots list contains duplicate timeslot(s).";
+
+    private final List<JsonAdaptedTimeslot> timeslots = new ArrayList<>();
+
+    /**
+     * Constructs a {@code JsonSerializableTimeslots} with the given timeslots.
+     */
+    @JsonCreator
+    public JsonSerializableTimeslots(@JsonProperty("timeslots") List<JsonAdaptedTimeslot> timeslots) {
+        if (timeslots != null) {
+            this.timeslots.addAll(timeslots);
+        }
+    }
+
+    /**
+     * Converts a given {@code ReadOnlyTimeslots} into this class for Jackson use.
+     *
+     * @param source future changes to this will not affect the created {@code JsonSerializableTimeslots}.
+     */
+    public JsonSerializableTimeslots(ReadOnlyTimeslots source) {
+        timeslots.addAll(source.getTimeslotList().stream()
+                .map(JsonAdaptedTimeslot::new)
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * Converts this timeslots into the model's {@code Timeslots} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated.
+     */
+    public Timeslots toModelType() throws IllegalValueException {
+        Timeslots modelTimeslots = new Timeslots();
+        for (JsonAdaptedTimeslot jsonAdaptedTimeslot : timeslots) {
+            Timeslot timeslot = jsonAdaptedTimeslot.toModelType();
+            if (modelTimeslots.hasTimeslot(timeslot)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_TIMESLOT);
+            }
+            modelTimeslots.addTimeslot(timeslot);
+        }
+        return modelTimeslots;
+    }
+
+}

797f87f7807af96dcb5ca4ab2e05206307e96627
thomas5564 <thomas.chooyankai@gmail.com>
2025-10-21 21:17:23 +0800

    Make exercise tracker a exercise arraylist wrapper

---
+++ src/main/java/seedu/address/model/person/Exercise.java
@@ -0,0 +1,45 @@
+package seedu.address.model.person;
+
+import java.util.Objects;
+
+/**
+ * Represents an exercise
+ */
+public class Exercise {
+    private Status status;
+    private final int number;
+    /**
+     * Creates an exercise object with the following parameters
+     * @param number is the exercise number
+     * @param status is the status of the exercise
+     */
+    public Exercise(int number, Status status) {
+        this.number = number;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ex %d: %s", number, status);
+    }
+
+    public void markStatus(Status status) {
+        this.status = status;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Exercise)) {
+            return false;
+        }
+        Exercise otherExercise = (Exercise) other;
+        return number == otherExercise.number
+                && Objects.equals(status, otherExercise.status);
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+}

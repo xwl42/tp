b231a878474effabfbc77e59ecaafbd26b4562d6
thomas5564 <thomas.chooyankai@gmail.com>
2025-10-24 14:39:26 +0800

    Update mark attendance test

--- src/test/java/seedu/address/logic/commands/MarkAttendanceTest.java
+++ src/test/java/seedu/address/logic/commands/MarkAttendanceTest.java
@@ -15,7 +15,6 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.MultiIndex;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -42,7 +41,7 @@ public class MarkAttendanceTest {
                 new MultiIndex(INDEX_FIRST_PERSON), INDEX_FIRST_LAB, true);
 
         String expectedMessage = String.format(MarkAttendanceCommand.MESSAGE_MARK_ATTENDANCE_SUCCESS,
-                INDEX_FIRST_LAB.getOneBased(), Messages.format(editedPerson));
+                INDEX_FIRST_LAB.getOneBased(), firstPerson.getName());
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(firstPerson, editedPerson);
@@ -65,7 +64,7 @@ public class MarkAttendanceTest {
     }
 
     @Test
-    public void execute_labAlreadyMarked_throwsCommandException() {
+    public void execute_labAlreadyMarked_compilesMessageSuccessfully() throws CommandException {
         Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         LabAttendanceList labAttendanceList = new LabList();
         labAttendanceList.markLabAsAttended(0);
@@ -75,8 +74,15 @@ public class MarkAttendanceTest {
 
         MarkAttendanceCommand markAttendanceCommand = new MarkAttendanceCommand(
                 new MultiIndex(INDEX_FIRST_PERSON), INDEX_FIRST_LAB, true);
-        assertThrows(CommandException.class, () -> markAttendanceCommand.execute(model));
+
+        CommandResult result = markAttendanceCommand.execute(model);
+        String expectedMessage = String.format(
+                MarkAttendanceCommand.MESSAGE_FAILURE_ALREADY_ATTENDED,
+                INDEX_FIRST_LAB.getOneBased(),
+                firstPerson.getName());
+        assertEquals(expectedMessage.trim(), result.getFeedbackToUser().trim());
     }
+
 
     @Test
     public void equals() {
